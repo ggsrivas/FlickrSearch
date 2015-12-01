2a7e27dac2a09d435a4728fcbe1a12aa5cb265c7
opendriod <ajaythakur@hotmail.com>
2015-11-30 20:50:34 -0800

    Initial Commit

---
+++ FlickrSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrSearch
+//
+//  Created by Ajay Thakur on 11/30/15.
+//  Copyright © 2015 Ajay Thaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlickrSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrSearch
+//
+//  Created by Ajay Thakur on 11/30/15.
+//  Copyright © 2015 Ajay Thaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
